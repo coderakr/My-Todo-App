1e943a4b6c6bb058773dde4ff42079dd3ef012d4
coderakr <139627558+coderakr@users.noreply.github.com>
2026-02-04 22:30:31 +0530

    update 4

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -67,8 +67,8 @@ export const login = async (req, res) => {
     // Save JWT in cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
@@ -92,8 +92,8 @@ export const logout = (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
     });
 
     return res.status(200).json({
